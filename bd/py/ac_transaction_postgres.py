2dc0dd4acdeff5511ff6d3a8ab0345bccbba8a34
erick.jose.heiler@gmail.com <erick.jose.heiler@gmail.com>
2022-12-18 22:45:40 -0300

    adicao do postgres

--- bd/py/ac_transaction_postgres.py
+++ bd/py/ac_transaction_postgres.py
@@ -6,8 +6,8 @@ import time
 mydb = psycopg2.connect(
   host="localhost",
   user="postgres",
-  password="root",
-  database="employees"
+  password="Alves@2811",
+  database="chicago"
 )
 
 # qual tabela utilizar?
@@ -37,26 +37,35 @@ else:
     i = 0
 
 c_ou_d = lista_c_ou_d[i][0]
-print("escolhi: "+c_ou_d, end="", flush=True)
+#print("escolhi: "+c_ou_d, end="", flush=True)
 
 # deixa commit automático (0) ou controla via transação (1)?
 # se deixar commit automático (1), os clientes executam simultaneamente podem incorrer em conflito
 # se controlar via transação, ocorre o bloqueio pelo BD
 
-commit_automatico = True
-#commit_automatico = False
+#commit_automatico = True
+commit_automatico = False
 if commit_automatico:
   pass
 else:
   cursor.execute("begin")
 
+'''commit_automatico = True
+#commit_automatico = False
+if commit_automatico:
+  cursor.execute("set autocommit=1")
+else:
+  cursor.execute("set autocommit=0")
+  cursor.execute("start transaction") # não precisaria deste comando, pois autocommit=0 já inicia uma transação
+
+'''
 # obtém os colaboradores do cargo
 # consulta parametrizada pois existem dados com aspas simples
 sql = f"select * from {tabela} where {campo} = %s"
 
 # DESEJA FAZER CONTROLE DE CONCORRÊNCIA VIA BLOQUEIO?
-#bloqueio = True
-bloqueio = False
+bloqueio = True
+#bloqueio = False
 if bloqueio:
     sql += " FOR UPDATE"
 
@@ -65,27 +74,30 @@ cursor.execute(sql, [c_ou_d])
 # obtém os resultados
 colabs = cursor.fetchall()
 
-# escolhe dois colaboradores
+'''# escolhe dois colaboradores
 i = random.randint(0, len(colabs)-1)
 j = i
 while i == j:
   j = random.randint(0, len(colabs)-1)
 alguem = colabs[i]
-outro = colabs[j]
+outro = colabs[j]'''
 
+alguem = colabs[i]
+depto = 'POLICE'
 # verifica qual o tipo de salário
 if alguem[4] == "Salary":
   campo_update = "salario_anual"
 else:
   campo_update = "valor_hora"
 # prepara o sql
-sql = f"update {tabela} set {campo_update} = {campo_update} * 0.1 where nome = '{alguem[0]}'"
+sql = f"update {tabela} set {campo_update} = {campo_update} * 1.1 where departamento = '{depto}'"
 # dá o aumento para esse(a) colaborador(a) :-)
 cursor.execute(sql)
 
+print("Departamento: ", depto)
 # espera um tempo, aqui é para caracterizar a chance de conflito >-) se estiver sem controle de concorrência
-time.sleep(2)
-
+#time.sleep(2)
+'''
 # REPETE PARA O OUTRO
 # verifica qual o tipo de salário
 if outro[4] == "Salary":
@@ -95,14 +107,14 @@ else:
 # prepara o sql
 sql = f"update {tabela} set {campo_update} = {campo_update} * 0.1 where nome = '{outro[0]}'"
 # dá o aumento para esse(a) colaborador(a) :-)
-cursor.execute(sql)
+cursor.execute(sql)'''
 
 if not commit_automatico:
   # efetiva a transação
   cursor.execute("commit")
 
 # mostra o resultado
-print(f" => {alguem[0]} e {outro[0]}  ganharam :-)")
+#print(f" => {alguem[0]} e {outro[0]}  ganharam :-)")
 
 # referências:
 # https://www.postgresql.org/docs/current/explicit-locking.html#LOCKING-ROWS
